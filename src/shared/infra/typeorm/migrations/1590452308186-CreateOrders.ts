6dfd37b9bac1d66622577d6556329fdeab22ebfe
Diogo Naka <diogoshindhinaka@gmail.com>
2020-06-20 21:19:09 -0300

    create migrations

---
+++ src/shared/infra/typeorm/migrations/1590452308186-CreateOrders.ts
@@ -0,0 +1,52 @@
+import { Table, QueryRunner, MigrationInterface } from 'typeorm';
+
+export default class CreateOrders1590452308186 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'customer_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+
+        foreignKeys: [
+          {
+            name: 'OrderCustomer',
+            columnNames: ['customer_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'customers',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'OrderCustomer');
+
+    await queryRunner.dropTable('orders');
+  }
+}
---
+++ src/shared/infra/typeorm/migrations/1592088266048-CreateOrdersProducts.ts
@@ -0,0 +1,76 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrdersProducts1592088266048
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'product_id',
+            type: 'uuid',
+          },
+          {
+            name: 'order_id',
+            type: 'uuid',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+
+        foreignKeys: [
+          {
+            name: 'OrderProducts',
+            columnNames: ['product_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'products',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'ProductOrders',
+            columnNames: ['order_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'orders',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'OrderProducts');
+
+    await queryRunner.dropForeignKey('orders_products', 'ProductOrders');
+
+    await queryRunner.dropTable('orders_products');
+  }
+}
